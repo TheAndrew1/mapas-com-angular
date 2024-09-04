7c0966bce911e1b9bf898fc2ce19c0a375f8dde0
Andre <andre.velasco@itaipuparquetec.org.br>
2024-09-04 16:54:48 -0300

    mapa inicial maplibre

--- src/app/components/map-libre/map-libre.component.ts
+++ src/app/components/map-libre/map-libre.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import maplibregl from 'maplibre-gl';
 
 @Component({
   selector: 'app-map-libre',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './map-libre.component.css'
 })
 export class MapLibreComponent {
-
+  map!: any;
+  ngOnInit(){
+    this.map = new maplibregl.Map({
+      container: 'map', // container id
+      style: 'https://demotiles.maplibre.org/style.json', // style URL
+      center: [0, 0], // starting position [lng, lat]
+      zoom: 5// starting zoom
+    });
+  }
 }
