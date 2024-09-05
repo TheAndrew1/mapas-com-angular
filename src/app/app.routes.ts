eb5932d043c0fb70b1721c2d272ab9eeada626bb
Andre <andre.velasco@itaipuparquetec.org.br>
2024-09-05 12:00:13 -0300

    tirei o maplibre, vou focar no uso do leaflet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import { MapLibreComponent } from './components/map-libre/map-libre.component';
 import { LeafletComponent } from './components/leaflet/leaflet.component';
 
 export const routes: Routes = [
-    {path: "map-libre", component: MapLibreComponent, pathMatch: 'full'},
     {path: "leaflet", component: LeafletComponent, pathMatch: 'full'},
-    {path: "*", redirectTo: "/"}
+    {path: "*", redirectTo: "leaflet"}
 ];
